33fdf4deaffbaa390fe17088e0f3a1b4153c7dcf
SujalaBafna <150019272+SujalaBafna@users.noreply.github.com>
2024-01-22 13:04:04 +0530

    Add files via upload

---
+++ Fibonacci numbers.py
@@ -0,0 +1,19 @@
+a=0
+b=1
+print(a)
+print(b)
+while a>=0:
+    c= a+b
+    print(c)
+    a=b
+    b=c
+    if c>144:
+        print(".")
+        print(".")
+        print(".")
+        break
+    
+
+
+
+
